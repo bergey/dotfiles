92753fc1421e4ea4d44edcac4bda3e67537e6a41
Daniel Bergey <bergey@teallabs.org>
2015-12-14 09:32:55 -0500

    bind { to insert newlines, indent
    my-c-mode-insert-lcurly does not insert '}' also, because I also use
    smartparens-mode, which inserts the '}'.

--- local/dmb-coding.el
+++ local/dmb-coding.el
@@ -198,4 +198,17 @@
         helm-gtags-mode
         dmb-company-short-idle))
 
+(defun my-c-mode-insert-lcurly ()
+  (interactive)
+  (insert "{")
+  (let ((pps (syntax-ppss)))
+    (when (and (eolp) (not (or (nth 3 pps) (nth 4 pps)))) ;; EOL and not in string or comment
+      (c-indent-line)
+      (insert "\n\n}")
+      (c-indent-line)
+      (forward-line -1)
+      (c-indent-line))))
+
+(define-key c-mode-base-map "{" 'my-c-mode-insert-lcurly)
+
 (provide 'dmb-coding)
