0f714742d3d0f4d49d2bda77f1e18c01bb869a82
Daniel Bergey <bergey@teallabs.org>
2016-05-08 07:54:41 -0400

    package: don't use melpa stable
    Not helpful unless I'm willing to add a bunch of pins.

--- local/dmb-package.el
+++ local/dmb-package.el
@@ -2,8 +2,8 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("stable" . "http://stable.melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 (measure-time "package-initialize" (package-initialize))
