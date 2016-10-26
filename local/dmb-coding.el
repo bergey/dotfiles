486dc4fe83292d7ba8fdc4f9d8874b7527f79465
Daniel Bergey <daniel.bergey@jet.com>
2016-10-26 09:39:48 -0400

    bind C-% to something like vim's %
    paren matching
    M-C is hard to type & unreliable with my keyboard

--- local/dmb-coding.el
+++ local/dmb-coding.el
@@ -5,6 +5,17 @@
 (setq-default tab-width 4)
 (setq indent-tabs-mode nil)
 
+;; https://www.emacswiki.org/emacs/NavigatingParentheses#toc2
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        ;; (t (self-insert-command (or arg 1)))
+        ))
+(bind-key "C-%" 'goto-match-paren)
+
 (use-package yasnippet
   :ensure yasnippet
   :diminish yas-minor-mode
