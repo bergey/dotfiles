8b63338a4f6b51c6db707175771c43ab69e9db30
Daniel Bergey <daniel.bergey@jet.com>
2017-02-28 13:49:01 -0500

    org - adjust TODO state colors

--- local/dmb-org.el
+++ local/dmb-org.el
@@ -135,12 +135,15 @@
 ;; WISH - review these periodically, might go back on the TODO list
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WIP(p)" "|" "DONE(d!)")
-        (type "DELAY(l)" "BLOCKED(b)" "|" "PR(g)"  "WISH(w)" "CANCEL(k!)")))
+        (type "DELAY(l)" "BLOCKED(b)" "PR(g)" "|"  "WISH(w)" "CANCEL(k!)")))
 
 (setq org-todo-keyword-faces
       '(("TODO" . (:foreground "light gray" :weight bold))
         ("WISH" . (:foreground "deep sky blue" :weight bold))
         ("DELAY" . (:foreground "chocolate" :weight bold))
+        ("BLOCKED" . (:foreground "chocolate" :weight bold))
+        ("PR" . (:foreground "chocolate" :weight bold))
+        ("WIP" . (:foreground "magenta" :weight bold))
         ))
 
 ;; (setq org-tags-exclude-from-inheritance '("project"))
