61c024e798588ccdd2c268f3147ebfeaf5f0f293
Daniel Bergey <daniel.bergey@jet.com>
2017-07-04 08:36:13 -0400

    evil: handle more emacs/vi collisions
    - defer to vi for some where I know the vi bindings
    - fresh bindings for a few functions I like in evil

--- local/dmb-evil.el
+++ local/dmb-evil.el
@@ -9,7 +9,7 @@
             (define-key evil-insert-state-map (kbd key-string) nil)
             (define-key evil-visual-state-map (kbd key-string) nil)
             )
-          '("C-b" "C-d" "C-e" "C-f" "C-k" "C-n" "C-o" "C-p" "C-v" "C-w" "C-y" "C-."))
+          '("C-a" "C-d" "C-e" "C-k" "C-o" "C-v" "C-w" "C-y" "C-."))
 
   (setcdr evil-insert-state-map nil)
 
@@ -17,6 +17,16 @@
   (define-key evil-insert-state-map (kbd evil-toggle-key) 'evil-emacs-state)
   (define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
   (define-key evil-normal-state-map (kbd "Q") 'call-last-kbd-macro)
-  (define-key evil-visual-state-map (kbd "Q") 'call-last-kbd-macro))
+  (define-key evil-visual-state-map (kbd "Q") 'call-last-kbd-macro)
+
+  ;; new bindings
+  (bind-key "M-<down>" 'evil-scroll-line-down evil-motion-state-map)
+  (bind-key "M-<up>" 'evil-scroll-line-up evil-motion-state-map)
+
+  ;;; per-mode customization
+  ;; (evil-set-initial-state 'org-capture-mode 'insert) ;; did not work 2017-07-03
+  (add-hook 'org-capture-mode-hook 'evil-insert-state)
+
+  )
 
 (provide 'dmb-evil)
