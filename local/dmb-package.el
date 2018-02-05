e18ed3ef5b416a4107c61ea5c3f298874c0ea86d
Daniel Bergey <bergey@teallabs.org>
2018-02-05 16:00:16 -0500

    remove some more `package.el` bootstrapping

--- local/dmb-package.el
+++ local/dmb-package.el
@@ -1,16 +1,9 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-;; (add-to-list 'package-archives
-;;              '("stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 (measure-time "package-initialize" (package-initialize))
 
-(when (not (package-installed-p 'use-package))
-  (package-install 'use-package))
 (eval-when-compile (require 'use-package))
 
 (setq use-package-verbose t)
