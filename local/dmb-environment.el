02be85426af595f653de9f4484e166604ba2b2e6
Daniel Bergey <bergey@teallabs.org>
2018-02-14 11:51:19 -0500

    more useful elisp "prelude" libraries - f, dash-functional

--- local/dmb-environment.el
+++ local/dmb-environment.el
@@ -1,7 +1,9 @@
 ;; elisp libraries & variables
 ;; loaded after use-package but before anything else
 
+(use-package f)
 (use-package dash)
+(use-package dash-functional)
 (use-package s)
 
 (use-package exec-path-from-shell
