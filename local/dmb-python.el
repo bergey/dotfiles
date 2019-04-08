8c65a610556adef5e74334b797c2e0921db132fe
Daniel Bergey <bergey@teallabs.org>
2019-04-08 14:51:30 -0400

    python: `--simple-prompt` for `ipython`

--- local/dmb-python.el
+++ local/dmb-python.el
@@ -1,6 +1,7 @@
 (use-package python
   :config
   (setq python-shell-interpreter "ipython")
+  (setq python-shell-interpreter-args "--simple-prompt -i")
   (setq python-mode-hook '(
                            flycheck-mode
                            whitespace-mode
