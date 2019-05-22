32b3618607f3d0d75bcf796157e5c0248ad36e13
Daniel Bergey <bergey@teallabs.org>
2019-05-22 11:18:14 -0400

    use-package sql, so we load before looking for variables

--- local/dmb-sql.el
+++ local/dmb-sql.el
@@ -1,66 +1,68 @@
-;; matches \set PROMPT1 '%`date +%H:%M:%S`%R%# ' in ~/.psqlrc
-(sql-set-product-feature 'postgres :prompt-regexp "^[_:[:alnum:]]*[=][#>] ")
+(use-package sql
+  :config
+  ;; matches \set PROMPT1 '%`date +%H:%M:%S`%R%# ' in ~/.psqlrc
+  (sql-set-product-feature 'postgres :prompt-regexp "^[_:[:alnum:]]*[=][#>] ")
 
-(setq sql-connection-alist
-      '((logicalbuildings (sql-product 'postgres)
-                          (sql-user "bergey")
-                          (sql-database "data_api_prod")
-                          (sql-server "localhost")
-                          (sql-port 3306))
-        (logicalbuildings-su (sql-product 'postgres)
-                          (sql-user "su_bergey")
-                          (sql-database "data_api_prod")
-                          (sql-server "localhost")
-                          (sql-port 3306))
-        (docker-localhost (sql-product 'postgres)
-                          (sql-user "postgres")
-                          (sql-database "postgres")
-                          (sql-server "localhost")
-                          (sql-port 5432))))
+  (setq sql-connection-alist
+        '((logicalbuildings (sql-product 'postgres)
+                            (sql-user "bergey")
+                            (sql-database "data_api_prod")
+                            (sql-server "localhost")
+                            (sql-port 3306))
+          (logicalbuildings-su (sql-product 'postgres)
+                               (sql-user "su_bergey")
+                               (sql-database "data_api_prod")
+                               (sql-server "localhost")
+                               (sql-port 3306))
+          (docker-localhost (sql-product 'postgres)
+                            (sql-user "postgres")
+                            (sql-database "postgres")
+                            (sql-server "localhost")
+                            (sql-port 5432))))
 
-(defun sql-logicicalbuildings ()
-  (interactive)
-  (password-store-copy "ets-postgresql-bergey")
-  (sql-connect 'logicalbuildings))
-
-(defun sql-logicalbuildings-su ()
+  (defun sql-logicicalbuildings ()
     (interactive)
-  (password-store-copy "ets-postgresql-bergey")
-  (sql-connect 'logicalbuildings-su))
+    (password-store-copy "ets-postgresql-bergey")
+    (sql-connect 'logicalbuildings))
 
-(defvar my-sql-replacements nil)
-(make-variable-buffer-local 'my-sql-replacements)
+  (defun sql-logicalbuildings-su ()
+    (interactive)
+    (password-store-copy "ets-postgresql-bergey")
+    (sql-connect 'logicalbuildings-su))
 
-(defun bergey/sql-send-buffer-replace ()
-  (interactive)
-  (let ((string (buffer-substring-no-properties (point-min) (point-max))))
-    (while (string-match "[$][0-9]+" string)
-      (let* ((placeholder (match-string 0 string))
-             (replacement (or (cdr (assoc placeholder my-sql-replacements))
-                              (read-string (format "Replacement for %s: " placeholder)))))
-        (unless (assoc placeholder my-sql-replacements)
-          (push (cons placeholder replacement) my-sql-replacements))
-        (setq string (replace-regexp-in-string (regexp-quote placeholder) replacement string))))
-    (sql-send-string string)))
+  (defvar my-sql-replacements nil)
+  (make-variable-buffer-local 'my-sql-replacements)
 
-
-(defun bergey/sql-send-string-replace ()
-  (interactive)
-  ;; TODO handle \" in quoted strings
-  (save-excursion
-    (let* ((string (buffer-substring-no-properties
-                    (progn (search-backward "\"") (forward-char) (point))
-                    (progn (search-forward "\"") (backward-char) (point))))
-          (sql-replacements nil))
+  (defun bergey/sql-send-buffer-replace ()
+    (interactive)
+    (let ((string (buffer-substring-no-properties (point-min) (point-max))))
       (while (string-match "[$][0-9]+" string)
         (let* ((placeholder (match-string 0 string))
-               (replacement (or (cdr (assoc placeholder sql-replacements))
+               (replacement (or (cdr (assoc placeholder my-sql-replacements))
                                 (read-string (format "Replacement for %s: " placeholder)))))
-          (unless (assoc placeholder sql-replacements)
-            (push (cons placeholder replacement) sql-replacements))
+          (unless (assoc placeholder my-sql-replacements)
+            (push (cons placeholder replacement) my-sql-replacements))
           (setq string (replace-regexp-in-string (regexp-quote placeholder) replacement string))))
-      (sql-send-string string))))
+      (sql-send-string string)))
 
-(define-key sql-interactive-mode-map (kbd "C-c C-w") nil)
+
+  (defun bergey/sql-send-string-replace ()
+    (interactive)
+    ;; TODO handle \" in quoted strings
+    (save-excursion
+      (let* ((string (buffer-substring-no-properties
+                      (progn (search-backward "\"") (forward-char) (point))
+                      (progn (search-forward "\"") (backward-char) (point))))
+             (sql-replacements nil))
+        (while (string-match "[$][0-9]+" string)
+          (let* ((placeholder (match-string 0 string))
+                 (replacement (or (cdr (assoc placeholder sql-replacements))
+                                  (read-string (format "Replacement for %s: " placeholder)))))
+            (unless (assoc placeholder sql-replacements)
+              (push (cons placeholder replacement) sql-replacements))
+            (setq string (replace-regexp-in-string (regexp-quote placeholder) replacement string))))
+        (sql-send-string string))))
+
+  (define-key sql-interactive-mode-map (kbd "C-c C-w") nil))
 
 (provide 'dmb-sql)
