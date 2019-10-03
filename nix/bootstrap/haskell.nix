4d1dc6acbcdfc7420875ad7c903aabc599a5b9a4
Daniel Bergey <bergey@teallabs.org>
2019-10-03 16:15:13 +0000

    pin Haskell bootstrap to a version where cabal2nix builds

--- nix/bootstrap/haskell.nix
+++ nix/bootstrap/haskell.nix
@@ -1,14 +1,16 @@
 let
-    nixpkgs =
-        let snapshot = builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json);
-        inherit (snapshot) owner repo rev;
-        in builtins.fetchTarball {
-            inherit (snapshot) sha256;
-            url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-            };
-    pkgs = import nixpkgs { config = {
-    allowBroken = true;
-}; };
+    nixpkgs = let
+        # let snapshot = builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json);
+        # inherit (snapshot) owner repo rev;
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "d484f2b7fc0834a068e8ace851faa449a03963f5";
+      in builtins.fetchTarball {
+        # inherit (snapshot) sha256;
+        sha256 = "0jk93ikryi2hqc30l2n5i4vlgmklrlzb8cf7b3sg1q3k70q344jn";
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        };
+    pkgs = import nixpkgs {};
 
     mkEnv = env: if pkgs.lib.inNixShell
         then pkgs.mkShell (env // {buildInputs = env.paths;})
