620146d701b282b71e1318b5127526b05459dafc
Daniel Bergey <bergey@teallabs.org>
2019-12-07 08:39:25 -0500

    haskell-env: use usual nix snapshot

--- nix/bootstrap/haskell.nix
+++ nix/bootstrap/haskell.nix
@@ -1,13 +1,9 @@
 let
-    nixpkgs = let
-        # let snapshot = builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json);
-        # inherit (snapshot) owner repo rev;
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "d484f2b7fc0834a068e8ace851faa449a03963f5";
+    nixpkgs = # let
+        let snapshot = builtins.fromJSON (builtins.readFile ../nixpkgs-snapshot.json);
+        inherit (snapshot) owner repo rev;
       in builtins.fetchTarball {
-        # inherit (snapshot) sha256;
-        sha256 = "0jk93ikryi2hqc30l2n5i4vlgmklrlzb8cf7b3sg1q3k70q344jn";
+        inherit (snapshot) sha256;
         url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
         };
     pkgs = import nixpkgs {};
