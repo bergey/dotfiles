432da9c6c960061bf7295169f30750a227c3e4ac
Daniel Bergey <bergey@teallabs.org>
2019-12-10 02:05:34 +0000

    ledger: tab completion

--- emacs/local/dmb-ledger.el
+++ emacs/local/dmb-ledger.el
@@ -1,20 +1,23 @@
 (use-package ledger-mode :ensure t
   :mode "\\.ledger\\'"
-  :config (progn
-            (bind-key "M-m" 'ledger-set-month ledger-mode-map)
-            (setq ledger-reports
-                  '(("bal" "ledger -f %(ledger-file) bal")
-                    ("reg" "ledger -f %(ledger-file) reg")
-                    ("payee" "ledger -f %(ledger-file) reg -- %(payee)")
-                    ("account" "ledger -f %(ledger-file) reg %(account)")
-                    ("assets" "ledger -f %(ledger-file) --price-db prices.db -s bal assets liabilities -V")
-                    ("expenses" "ledger -f %(ledger-file) -s bal expenses --begin 2015")
-                    ("cleared" "ledger -f %(ledger-file) -s bal -C assets liabilities")
-                    ("brokerage" "ledger -f %(ledger-file) bal fidelity")
-                    ("monthly" "ledger -f %(ledger-file) reg expenses -M --period-sort \"(-amount)\"")
-                    ("income" "ledger -f bergey.ledger -Y  register '^Income:[^S]' --begin=$(date +%Y)-01-01")
-                    ("tikun" "ledger -f %(ledger-file) reg expenses:tikun --begin=$(date +%Y)-01-01")
-                    ))))
+  :config
+  (bind-key "M-m" 'ledger-set-month ledger-mode-map)
+  (setq ledger-reports
+        '(("bal" "ledger -f %(ledger-file) bal")
+          ("reg" "ledger -f %(ledger-file) reg")
+          ("payee" "ledger -f %(ledger-file) reg -- %(payee)")
+          ("account" "ledger -f %(ledger-file) reg %(account)")
+          ("assets" "ledger -f %(ledger-file) --price-db prices.db -s bal assets liabilities -V")
+          ("expenses" "ledger -f %(ledger-file) -s bal expenses --begin 2015")
+          ("cleared" "ledger -f %(ledger-file) -s bal -C assets liabilities")
+          ("brokerage" "ledger -f %(ledger-file) bal fidelity")
+          ("monthly" "ledger -f %(ledger-file) reg expenses -M --period-sort \"(-amount)\"")
+          ("income" "ledger -f bergey.ledger -Y  register '^Income:[^S]' --begin=$(date +%Y)-01-01")
+          ("tikun" "ledger -f %(ledger-file) reg expenses:tikun --begin=$(date +%Y)-01-01")
+          ))
+  (add-to-list 'ledger-mode-hook
+               (lambda () (setq-local tab-always-indent 'complete)))
+  )
 
 (defun date-format-usa-to-iso ()
   (interactive)
