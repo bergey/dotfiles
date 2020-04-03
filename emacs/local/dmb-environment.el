9f236e011cd50b669ce4dbdab9481379d3be048b
Daniel Bergey <bergey@simspace.com>
2020-04-03 08:44:43 -0400

    remove direnv

--- emacs/local/dmb-environment.el
+++ emacs/local/dmb-environment.el
@@ -11,9 +11,4 @@
  (when (memq window-system '(mac ns x))
    (exec-path-from-shell-initialize)))
 
-(use-package direnv
-  :config
-  (direnv-mode)                         ; global minor mode
-  )
-
 (provide 'dmb-environment)
