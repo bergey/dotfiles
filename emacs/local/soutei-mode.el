c7afe34cd5c246a7566f00775bc0e74db8375c9e
Daniel Bergey <bergey@simspace.com>
2020-08-04 03:53:17 -0400

    soutei: derive from prolog-mode, for indent & imenu

--- emacs/local/soutei-mode.el
+++ emacs/local/soutei-mode.el
@@ -1,5 +1,16 @@
 (defvar soutei-mode-hook nil)
 
+(defface soutei-keyword-face
+  '((t (:inherit font-lock-keyword-face)))
+  "Soutei keywords")
+
+(defface soutei-operator-face
+  '((t (:inherit font-lock-builtin-face)))
+  "Soutei builtin operators")
+
+(defvar soutei-indent-width prolog-indent-width
+  "indentation width used by auto-indent functions")
+
 (defvar soutei-mode-map (make-keymap)
   "Keymap for soutei major mode")
 
@@ -12,14 +23,6 @@
   "Syntax table for `soutei-mode'"
   )
 
-(defface soutei-keyword-face
-  '((t (:inherit font-lock-keyword-face)))
-  "Soutei keywords")
-
-(defface soutei-operator-face
-  '((t (:inherit font-lock-builtin-face)))
-  "Soutei builtin operators")
-
 (defconst soutei-font-lock-keywords
   '(
     (";.*" . 'font-lock-comment-face)
@@ -31,12 +34,13 @@
     ("\\?[a-zA-Z!@$%&*/<=>~_^][a-zA-Z0-9!@$%&*/<=>~_^?+-]*" . 'font-lock-variable-name-face)
     ))
 
-(define-derived-mode soutei-mode prog-mode "Soutei"
+(define-derived-mode soutei-mode prolog-mode "Soutei"
   "Major mode for editing Soutei files"
   (set-syntax-table soutei-mode-syntax-table)
   (set (make-local-variable 'comment-start) ";")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'comment-start-skip) ";+ *")
+  (set (make-local-variable 'prolog-indent-width) soutei-indent-width)
   (set 'font-lock-defaults '(soutei-font-lock-keywords)))
 
 (add-to-list 'auto-mode-alist '("\\.ava\\'" . soutei-mode))
