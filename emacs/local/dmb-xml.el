313cab551226ae414825bd81d172ea27b954c23b
Daniel Bergey <bergey@simspace.com>
2021-01-13 11:14:54 -0500

    use web-mode for TS, tsx

--- emacs/local/dmb-xml.el
+++ emacs/local/dmb-xml.el
@@ -36,7 +36,7 @@
 
 ;; everything web: HTML,javascript, css
 (use-package web-mode :ensure t
-  :mode "\\.\\(p?html\\|tpl\\|php\\|erb\\|mustach\\|jsx?\\|json\\|s?css\\|sass\\|fsproj\\|csproj\\|xml\\|tmpl\\)\\'"
+  :mode "\\.\\(p?html\\|tsx?\\|tpl\\|php\\|erb\\|mustach\\|jsx?\\|json\\|s?css\\|sass\\|fsproj\\|csproj\\|xml\\|tmpl\\)\\'"
   :config
   (progn
     (define-key web-mode-map (kbd "C-c C-l") 'w3m-browse-current-buffer)
