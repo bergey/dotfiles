933b6b6d6e8c81a468c870840c78b57cd8c87753
Daniel Bergey <bergey@simspace.com>
2021-06-25 10:10:51 -0400

    split package groups by OS, server / workstation

--- nix/global.nix
+++ nix/global.nix
@@ -28,117 +28,145 @@ let
             });
 
     bootstrap = import ./bootstrap.nix {inherit pkgs;};
-
-in with pkgs;
-
-buildEnv {
-  name = "bergey-env";
-  paths= [
-    alacritty
-    ansible
-    aspell
-    aspellDicts.en
-    atool
-    bash-completion
-    bench
-    direnv
-    dtach
-    editorconfig-core-c
-    git
-    git-lfs
-    gitAndTools.hub
-    gnumake
-    gnupg
-    gphoto2
-    graphviz
-    (haskell.lib.dontCheck haskellPackages.hasktags)
-    haskellPackages.pandoc
-    haskellPackages.hlint
-    haskellPackages.graphmod
-    # haskellPackages.HaRe
-    (haskellPackages.callPackage sizes {})
-    haskellPackages.wai-app-static # warp
-    htop
-    id3v2
-    imagemagick
-    inkscape
-    jq yq
-    keybase
-    kubectl
-    kubernetes-helm 
-    lftp
-    loc
-    # lrzip # broken 2021-03-03 MacOS
-    mr
-    msmtp
-    niv
-    nix-prefetch-git
-    nmap
-    nodePackages.jsonlint
-    pass
-    perlPackages.ImageExifTool
-    ripgrep
-    rsync
-    stack
-    stow
-    stylish-haskell
-    textql
-    tmux
-    # vagrant
-    w3m
-    wget
-    wireshark
-    xlsfonts
-    yaml2json
-    # python3.pkgs.yamllint # broken 2021-03-22
-
     # minimal derivation, ensures that we depend on all bootstrap envs
-    (derivation (bootstrap // {
+    bootstrap-prebuild = with pkgs; (derivation (bootstrap // {
       name = "bootstrap-envs";
       builder = "${bash}/bin/bash";
       args = [ "-c" "$coreutils/bin/mkdir $out; echo foo > $out/bootstrap-envs" ];
       system = builtins.currentSystem;
       inherit coreutils;
-    }))
+    }));
 
-    ] ++ (if stdenv.isDarwin then [
-      bashInteractive
-      govc
-      nix
-      # powershell # broken 2021-03-22
-      # it happens that my Mac is for work & my Linux boxen aren't
-      work.yarn
-      work.nodejs
-      work.postgresqlWithPackages
-      work.minio
-      work.zstd
-      work.rabbitmq-server
-    ] else [ # Linux
+    bergey = {
+      global = with pkgs; [
+        ansible
+        aspell
+        aspellDicts.en
+        atool
+        bash-completion
+        bench
+        direnv
+        dtach
+        editorconfig-core-c
+        git
+        git-lfs
+        gitAndTools.hub
+        gnumake
+        gnupg
+        gphoto2
+        graphviz
+        (haskell.lib.dontCheck haskellPackages.hasktags)
+        haskellPackages.pandoc
+        haskellPackages.hlint
+        haskellPackages.graphmod
+        # haskellPackages.HaRe
+        (haskellPackages.callPackage sizes {})
+        haskellPackages.wai-app-static # warp
+        htop
+        id3v2
+        imagemagick
+        jq yq
+        keybase
+        kubectl
+        kubernetes-helm 
+        lftp
+        loc
+        mr
+        msmtp
+        niv
+        nix-prefetch-git
+        nmap
+        nodePackages.jsonlint
+        pass
+        perlPackages.ImageExifTool
+        ripgrep
+        rsync
+        stack
+        stow
+        stylish-haskell
+        textql
+        tmux
+        # vagrant
+        w3m
+        wget
+        wireshark
+        xlsfonts
+        yaml2json
+        python3.pkgs.yamllint
+      ];
+
+      linux = with pkgs; [
         # (agda.withPackages (a: [ a.standard-library ]))
         acpi
-        arduino
         borgbackup
-        # calibre # broken apsw 2020-06-21
-        crawl
         curl
-        dmenu
         docker
-        dropbox-cli
-        feh
         file
-        # freeciv_gtk
         gitAndTools.git-annex
         google-cloud-sdk
         inotifyTools
-        isync # mbsync
         linuxPackages.virtualbox
+        lrzip # broken 2021-03-03 MacOS
+        psmisc # pstree &c
+      ];
+
+      darwin = with pkgs; [
+        bashInteractive
+        govc
+        nix
+        powershell
+      ];
+
+      simspace = with work; [
+        minio
+        nodejs
+        postgresqlWithPackages
+        rabbitmq-server
+        yarn
+        zstd
+      ];
+
+      workstation = with pkgs; [
+        inkscape
+      ];
+
+      linux-workstation = with pkgs; [
+        alacritty
+        arduino
+        # calibre # broken apsw 2020-06-21
+        crawl
+        dmenu
+        dropbox-cli
+        feh
+        # freeciv_gtk
         maim # screenshots
         pavucontrol
-        psmisc # pstree &c
         slack
         transmission
         xorg.xev
         zathura
         # zoom-us
-    ]);
-  }
+      ];
+
+      linux-server = [];
+    };
+
+in rec {
+  linux-workstation = pkgs.buildEnv {
+    name = "bergey-linux-workstation";
+    paths = with bergey; global ++ linux ++ worstation ++ linux-workstation;
+  };
+
+  linux-server = pkgs.buildEnv {
+    name = "bergey-linux-server";
+    paths = with bergey; global ++ linux ++ linux-server;
+  };
+
+  darwin = pkgs.buildEnv {
+    name = "bergey-darwin";
+    # it happens that my Mac is for work & my Linux boxen aren't
+    paths = bergey.global ++ bergey.darwin ++ bergey.simspace;
+  };
+
+  default = if pkgs.stdenv.isDarwin then darwin else linux-workstation;
+}
