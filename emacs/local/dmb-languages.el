ea87c87210157d2c4874564f1a6545a6e4b7c3d2
Daniel Bergey <bergey@simspace.com>
2022-01-18 07:55:54 -0500

    rust: fix flycheck, bind `rust-format-buffer`
    until the PR merges to fix
    https://github.com/flycheck/flycheck/issues/1916

--- emacs/local/dmb-languages.el
+++ emacs/local/dmb-languages.el
@@ -28,9 +28,18 @@
 ;; rust
 (use-package rust-mode :ensure t
   :mode ("\\.rs\'" . rust-mode)
+  :bind (:map rust-mode-map
+              ("C-c C-," . rust-format-buffer))
   :config
   (use-package flycheck-rust :ensure t
     :config
+    ;; upstream doesn't cope when there's more on the line than the subcommand
+    (defun flycheck-rust-cargo-has-command-p (command)
+      (let ((cargo (funcall flycheck-executable-find "cargo")))
+        (member command
+          (mapcar
+           (lambda (s) (car (s-split-words s)))
+           (ignore-errors (process-lines cargo "--list"))))))
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
   (add-hook 'rust-mode-hook '(lambda ()  (flycheck-mode t))))
 
