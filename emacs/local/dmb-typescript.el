e8d5a7f0e1aeedcebe5def0d31296ce7fbab3b73
Daniel Bergey <bergey@simspace.com>
2022-04-03 08:51:39 -0400

    emacs: fix parens, use-package sections, reindent

--- emacs/local/dmb-typescript.el
+++ emacs/local/dmb-typescript.el
@@ -2,33 +2,33 @@
   :mode "\\.ts"
   :config
 
-(use-package tide :ensure t
-  :init
-  (defun setup-tide-mode ()
-    (interactive)
-    (tide-setup)
-    (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (eldoc-mode +1)
-    ;; (tide-hl-identifier-mode +1)
+  (use-package tide :ensure t
+    :init
+    (defun setup-tide-mode ()
+      (interactive)
+      (tide-setup)
+      (flycheck-mode +1)
+      (setq flycheck-check-syntax-automatically '(save mode-enabled))
+      (eldoc-mode +1)
+      ;; (tide-hl-identifier-mode +1)
+      )
+    :hook (before-save . tide-format-before-save)
+    :bind (:map tide-mode-map
+                ("C-c C-," . tide-format))
+    :config
+    (defun typescript-sort-imports ()
+      "sort the current region according to typescript import rules"
+      (interactive)
+      ;; TODO handle all import groups, regardless of position in buffer
+      (sort-regexp-fields nil "^.*$" "\".*\"" (point) (mark)))
+
+    (setq typescript-mode-hook
+          '(whitespace-mode
+            smartparens-strict-mode
+            dmb-company-short-idle
+            setup-tide-mode
+            ))
     )
-  :hook (before-save . tide-format-before-save))
-  :bind (:map tide-mode-map
-              ("C-c C-," . tide-format))
-  )
-
-  (defun typescript-sort-imports ()
-  "sort the current region according to typescript import rules"
-  (interactive)
-  ;; TODO handle all import groups, regardless of position in buffer
-  (sort-regexp-fields nil "^.*$" "\".*\"" (point) (mark)))
-
-  (setq typescript-mode-hook
-        '(whitespace-mode
-          smartparens-strict-mode
-          dmb-company-short-idle
-          setup-tide-mode
-          ))
   )
 
 (use-package add-node-modules-path
