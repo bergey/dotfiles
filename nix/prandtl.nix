498ddc135329b301dba9abd3f101301c773ece88
Daniel Bergey <bergey@simspace.com>
2022-04-10 02:04:09 +0000

    fresh install, version 21.11

--- nix/prandtl.nix
+++ nix/prandtl.nix
@@ -217,6 +217,6 @@ virtualisation.docker.enable = true;
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
+  system.stateVersion = "21.11"; # Did you read the comment?
 
 }
