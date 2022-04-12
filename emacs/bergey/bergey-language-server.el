5b12068df8a8c8943d8532487f206eef5a8e8b29
Daniel Bergey <bergey@simspace.com>
2022-04-12 17:53:35 -0400

    lsp: formatting

--- emacs/bergey/bergey-language-server.el
+++ emacs/bergey/bergey-language-server.el
@@ -7,9 +7,13 @@
 (use-package lsp-ui :ensure t
   :commands lsp-ui-mode
   :config
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-ui-sideline-enable nil)
-  (setq lsp-ui-doc-enable nil)
+  (setq
+   lsp-headerline-breadcrumb-enable nil
+   lsp-ui-sideline-enable nil
+   lsp-ui-doc-enable nil
+   gc-cons-threshold 100000000
+   read-process-output-max (* 1024 1024) ;; 1mb
+   )
   )
 
 (use-package lsp-ivy
