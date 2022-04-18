47d55f9900825f700c8ed1ad2190349bd71c28af
Daniel Bergey <bergey@simspace.com>
2022-04-18 10:57:35 -0400

    ruby: helpers for navigating & managing imports

--- emacs/bergey/bergey-ruby.el
+++ emacs/bergey/bergey-ruby.el
@@ -34,5 +34,35 @@
   (advice-add #'ruby-test-run-command :around #'amk-ruby-test-pretty-error-diffs-setup)
   )
 
+(defun bergey/ruby-navigate-imports ()
+  "move point to the beginning of the first import line"
+  (interactive)
+  (xref-push-marker-stack) ;; so we can return
+  (goto-char (point-min))
+  (re-search-forward "^\\(require\\|load\\)")
+  (goto-char (line-beginning-position))
+  )
+(bind-key "C-c i" #'bergey/ruby-navigate-imports ruby-mode-map)
+
+(defun bergey/ruby-yank-module-name ()
+  "copy the module name to the kill ring"
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (re-search-forward "^module +\\([a-zA-Z0-9.]+\\)")
+    (kill-new (match-string 1))
+    (message (match-string 1))
+    ))
+(bind-key "C-c m" #'bergey/ruby-yank-module-name ruby-mode-map)
+
+(defun bergey/ruby-yank-class-name  ()
+  "copy the class enclosing point to the kill ring"
+  (interactive)
+  (save-excursion
+    (re-search-backward "^ *class \\([a-zA-Z]+\\)")
+    (kill-new (match-string 1))
+    (message (match-string 1))
+    ) )
+(bind-key "C-c c" #'bergey/ruby-yank-class-name ruby-mode-map)
 
 (provide 'bergey-ruby)
