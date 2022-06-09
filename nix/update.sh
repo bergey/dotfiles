10843711b62be3e2cb592c67b630759327746587
Daniel Bergey <daniel.bergey@braze.com>
2022-06-09 10:09:47 -0400

    skip nix pin update if already on latest revision

--- nix/update.sh
+++ nix/update.sh
@@ -12,19 +12,23 @@ set -euxo pipefail
 
 # git diff-index --quiet HEAD -- || (echo "commit or stash changes"; exit 64)
 REV=$(curl -L https://nixos.org/channels/nixpkgs-unstable/git-revision)
-[ ! -d ~/code ] && mkdir ~/code
-[ ! -d ~/code/nixpkgs ] && git clone git@github.com:NixOS/nixpkgs.git
-cd ~/code/nixpkgs
-git fetch -a --quiet
-git checkout ${REV}
-SHA=$(nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/${REV}.tar.gz)
-cd -
-jq '{owner: "NixOS", repo: "nixpkgs", rev: $rev, sha256: $sha}' <<< '{}' \
-   --arg rev $REV --arg sha $SHA \
-    > nixpkgs-snapshot.json
-git reset # make sure we aren't commiting anything else
-git add nixpkgs-snapshot.json
-if ! git diff --cached --exit-code  --quiet; \
-    then git commit -m "$(basename $(pwd)): update nixpkgs snapshot"; \
-    else echo "nothing to commit"; \
-    fi
+if [ $(jq -r .rev nixpkgs-snapshot.json) = "$REV" ]
+  then echo 'already on latest revision'
+  else 
+    [ ! -d ~/code ] && mkdir ~/code
+    [ ! -d ~/code/nixpkgs ] && git clone git@github.com:NixOS/nixpkgs.git
+    cd ~/code/nixpkgs
+    git fetch -a --quiet
+    git checkout ${REV}
+    SHA=$(nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/${REV}.tar.gz)
+    cd -
+    jq '{owner: "NixOS", repo: "nixpkgs", rev: $rev, sha256: $sha}' <<< '{}' \
+      --arg rev $REV --arg sha $SHA \
+        > nixpkgs-snapshot.json
+    git reset # make sure we aren't commiting anything else
+    git add nixpkgs-snapshot.json
+    if ! git diff --cached --exit-code  --quiet; \
+        then git commit -m "$(basename $(pwd)): update nixpkgs snapshot"; \
+        else echo "nothing to commit"; \
+        fi
+fi
