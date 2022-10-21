d030c9b63f90269aa2cda23f977b5f44c0b880f6
Daniel Bergey <daniel.bergey@braze.com>
2022-10-21 12:50:17 -0400

    define sort-words

--- emacs/bergey/bergey-editing.el
+++ emacs/bergey/bergey-editing.el
@@ -130,4 +130,16 @@
 (setq hi-lock-face-defaults
       '("hi-green" "hi-blue" "hi-pink" "hi-aquamarine" "hi-salmon" "hi-yellow" ))
 
+;; https://www.emacswiki.org/emacs/SortWords
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 (provide 'bergey-editing)
