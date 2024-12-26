f6afe31c763c399b4a223024624786bbe4b1d79d
Daniel Bergey <daniel.bergey@braze.com>
2024-12-26 10:29:00 -0500

    nix: unpin wireshark

--- nix/global.nix
+++ nix/global.nix
@@ -21,15 +21,6 @@ let
       };
     in pkgs.rWrapper.override{ packages = with pkgs.rPackages; [ tidyverse promr ]; };
 
-    pinned_wireshark = let
-      pkgs = import ./nixpkgs.nix {
-        snapshot = { # 2024-11-20
-          rev = "8edf06bea5bcbee082df1b7369ff973b91618b8d";
-          sha256 = "0zwkwkiifcbzsmfn932nkgvhaj91n3hqg05fqss8s79bdwk6w35i";
-        };
-      };
-    in pkgs.wireshark;
-
     kits = {
       global = (with pkgs; [
         aspell
@@ -77,7 +68,7 @@ let
         typos
         watch
         wget
-        pinned_wireshark
+        wireshark
         wrk
         xlsfonts
         xsv
