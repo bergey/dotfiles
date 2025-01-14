724138310944a5889be4c9adaa5b1a5cb82d6247
Daniel Bergey <daniel.bergey@braze.com>
2025-01-14 12:18:47 +0000

    emacs: picklebush interactive mode

--- emacs/bergey/bergey-web.el
+++ emacs/bergey/bergey-web.el
@@ -149,29 +149,31 @@ by using nxml's indentation rules."
   "an assoc list whose keys are directory names and values are emacs process objects")
 
 (defun picklebush-find-or-start-process (dir)
-  (or (cdr (assoc directory picklebush-processes))
-      (let* ((name (format "picklebush %s" dir))
-             (buf  (get-buffer-create name))
-             (process (make-process
-                       :name name
-                       :command `("picklebush" "--dir" ,dir)
-                       :buffer buf)))
-        (push (cons dir process) picklebush-processes)
-        process)))
+  (or
+   (let ((process (cdr (assoc dir picklebush-processes))))
+     (if (and process (eq 'run (process-status process))) process))
+   (lexical-let ((process (make-process
+                           :name (format "picklebush %s" dir)
+                           :command `("picklebush" "--dir" ,dir)
+                           :stderr (get-buffer-create (format "picklebush errors %s" dir))
+                           :buffer nil)))
+     (push (cons dir process) picklebush-processes)
+     ;; set up the callback before we send our query
+     (set-process-filter process
+                         (lambda (p response)
+                           (if (equal p process) ; ignore stderr
+                               (let* ((file-line (s-split "::" response))
+                                      (file (car file-line))
+                                      (line (string-to-number (cadr file-line))))
+                                 (find-file-other-window file)
+                                 (goto-char (point-min))
+                                 (beginning-of-line line)))))
+     process)))
 
 (defun picklebush (text &optional dir)
   (interactive "Mtext to match:")
   (let* ((directory (or dir (projectile-project-root) (file-name-directory (buffer-file-name))))
          (pickle-process (picklebush-find-or-start-process directory)))
-    ;; set up the callback before we send our query
-    (set-process-filter pickle-process
-                        (lambda (p response)
-                          (let* ((file-line (s-split "::" response))
-                                 (file (car file-line))
-                                 (line (string-to-number (cadr file-line))))
-                            (find-file-other-window file)
-                            (goto-char (point-min))
-                            (beginning-of-line line))))
     (xref-push-marker-stack)
     ;; now send the query
     (cond
