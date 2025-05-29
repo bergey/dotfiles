e71fa7982a143692f3bdcafb6aeb3a471e972e98
Daniel Bergey <daniel.bergey@braze.com>
2025-05-29 10:43:23 -0400

    emacs mode for circuit netlists [wip]
    at least the dialect used in 6.004

---
+++ emacs/bergey/netlist-mode.el
@@ -0,0 +1,32 @@
+(defvar netlist-mode-hook
+  '(
+    highlight-indent-guides-mode
+    ))
+
+(defvar netlist-mode-map (make-keymap))
+
+(defvar netlist-mode-syntax-table
+  (let ((synTable (make-syntax-table)))
+    (modify-syntax-entry ?\' "\"" synTable)
+    synTable))
+
+(defconst netlist-font-lock-keywords
+  '(
+    ("\\.[a-z]*" . font-lock-keyword-face)
+    ;; ("^ *X[[:alnum:]]*" . font-lock-variable-name-face)
+    ;; ,(rx line-start (* whitespace) "X" (* anychar) whitespace (group (+ alnum))) . (1 font-lock-function-call-face)
+    ))
+
+(define-derived-mode netlist-mode fundamental-mode "NL"
+  "Major mode for circuit netlists"
+  (set-syntax-table netlist-mode-syntax-table)
+  (setq-local font-lock-defaults '(netlist-font-lock-keywords))
+  (setq-local comment-start "*")
+  (setq-local comment-use-syntax t)
+  (setq-local syntax-propertize-function
+              (syntax-propertize-rules
+               ((rx line-start (* whitespace) (group "*")) (1 "<"))
+               ((rx (group "//")) (1 "<"))
+               ((rx (group "\n")) (1 ">"))
+               ))
+  )
