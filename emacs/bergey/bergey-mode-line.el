648cc4265d87fd834a6db7db9e5ee9872e19fc98
Daniel Bergey <daniel.bergey@braze.com>
2025-06-20 17:50:40 -0400

    emacs: move left what I don't want truncated
    simplify line indicator

--- emacs/bergey/bergey-mode-line.el
+++ emacs/bergey/bergey-mode-line.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
 (use-package diminish :ensure t
   :commands diminish
   )
@@ -6,17 +8,24 @@
   (setq mode-line-format (--remove (equal it '(vc-mode vc-mode)) mode-line-format))
   )
 
+(telephone-line-defsegment* bergey/telephone-line-position-segment (&optional lines columns)
+  "Optional args set padding on lines/columns."
+  (let* ((l (number-to-string (if lines lines 3)))
+         (c (number-to-string (if columns columns 3))))
+    (list (concat " %" l "l" ":%" c "c"))))
+
 (use-package telephone-line
   :custom
   (telephone-line-evil-use-short-tag t)
   (telephone-line-lhs
    '((nil telephone-line-evil-tag-segment)
-     (accent telephone-line-vc-segment telephone-line-erc-modified-channels-segment telephone-line-process-segment)
+     (accent bergey/telephone-line-position-segment)
      (nil telephone-line-projectile-buffer-segment)))
   (telephone-line-rhs
    '((nil telephone-line-flycheck-segment telephone-line-misc-info-segment)
-     (accent telephone-line-major-mode-segment)
-     (nil telephone-line-airline-position-segment)))
+     (accent telephone-line-vc-segment telephone-line-erc-modified-channels-segment telephone-line-process-segment)
+     (nil telephone-line-major-mode-segment)
+     ))
   :custom-face
   (mode-line ((t (:foreground "#ea84fe" :background "gray10"))))
   (mode-line-inactive ((t (:background "gray20"))))
