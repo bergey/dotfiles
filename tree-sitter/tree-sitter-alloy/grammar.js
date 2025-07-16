9683f106325043a8b1a1191a0653b7c24cd699d9
Daniel Bergey <daniel.bergey@braze.com>
2025-07-16 06:35:01 -0400

    fact, | quantification

--- tree-sitter/tree-sitter-alloy/grammar.js
+++ tree-sitter/tree-sitter-alloy/grammar.js
@@ -19,8 +19,11 @@ module.exports = grammar({
     /\s/,
     $.comment,
   ],
-  word: $ => $._identifier,
-  conflicts: $ => [[$.implies_else]],
+  word: $ => $._name,
+    conflicts: $ => [
+        [$.implies_else],
+        [$.name, $.qual_name] // TODO resolve this better
+    ],
 
   // see https://alloytools.org/spec.html
   rules: {
@@ -32,7 +35,7 @@ module.exports = grammar({
       optional("abstract"),
       field("mult", optional($.mult)),
       "sig",
-      field("name", comma_separated($.identifier)),
+      field("name", comma_separated($.name)),
       optional($.sig_extends),
       "{",
       optional(field("field", comma_separated($.field))),
@@ -40,21 +43,21 @@ module.exports = grammar({
       // optional block
     ),
     sig_extends: $ => choice(
-      seq("extends", $.identifier),
-      seq("in", $.identifier) // multiple allowed here?
+      seq("extends", $.name),
+      seq("in", $.name) // multiple allowed here?
     ),
     field: $ => seq(
       optional("var"),
       // TODO split out decl
-      field("name", $.identifier),  // TODO multiple
+      field("name", $.name),  // TODO multiple
       ":",
       optional($.mult),
-      $.identifier
+      $.name
     ),
 
     fact: $ => seq(
       "fact",
-      field("name", optional($.identifier)),
+      field("name", optional($.name)),
       $.block
     ),
 
@@ -63,16 +66,18 @@ module.exports = grammar({
       repeat($.expr),
       "}"
     ),
+    _block_or_bar: $ => prec.right(choice($.block, seq("|", $.expr))),
 
     // TODO make the syntax tree nicer – what nodes should be named?
     expr: $ => choice(
-      $.const, $.qualName, "this", // @name ?
-      // prec(21, seq(/~|^|\*/, $.expr)), // Grammar error: Unexpected rule ExpandRegex(Assertion)
+      $.const, $.qual_name, "this", // @name ?
+      prec(21, seq(choice("~", "^", "*"), $.expr)),
       prec(20, seq($.expr, token.immediate("'"))),
       prec.left(19, seq($.expr, ".", $.expr)),
       prec(18, seq($.expr, "[", comma_separated($.expr), "]")), // is expr[] allowed?
       prec.left(17, seq($.expr, /<:|:>/, $.expr)),
-      prec.left(16, seq($.expr, optional(choice($.mult, "set")), "->", $.expr)),
+      // prec.left(16, seq($.expr, optional(choice($.mult, "set")), "->", $.expr)), // parse errors, ambiguity?
+      prec.left(16, seq($.expr, "->", $.expr)), // omit mult / set for now
       prec.left(15, seq($.expr, "&", $.expr)),
       prec.left(14, seq($.expr, "++", $.expr)),
       prec(13, seq("#", $.expr)),
@@ -85,27 +90,32 @@ module.exports = grammar({
         choice("in", "=", "<", ">", "=<", ">="))),
         $.expr),
       ),
-      // TODO let, quant, {}
-      // TODO more logical operators
+      // TODO more logical operators, {}
       prec.left(6, seq($.expr, choice("&&", "and"), $.expr)),
       $.implies_else,
       prec.left(4, seq($.expr, choice("<=>", "iff"), $.expr)),
       prec.left(3, seq($.expr, choice("||", "or"), $.expr)),
-      // TODO let quant, ;
+      prec.left(2, seq(choice($.mult, $.quant), comma_separated($.decl), $._block_or_bar)),
+      // TODO let ;
+      // let let_decl
+      // braces decl
 
       seq("(", $.expr, ")"),
       $.block
     ),
     implies_else: $ => seq($.expr, choice("=>", "implies"), $.expr, "else", $.expr),
 
-
     const: _ => choice(/-?[0-9]+/, "none", "univ", "iden"),
 
-    _identifier: _ => /[A-Za-z_]+/, // TODO full character class
-    identifier: $ => $._identifier,
-    // Maybe it's better to have named identifier nodes when there are several?
-    qualName: $ => seq(optional("this/"), repeat(seq($._identifier, token.immediate("/"))), $._identifier),
+    _name: _ => /[A-Za-z_]+/, // TODO full character class
+    name: $ => $._name,
+    // Maybe it's better to have named name nodes when there are several?
+    qual_name: $ => seq(optional("this/"), repeat(seq($._name, token.immediate("/"))), $._name),
     mult: _ => choice("lone", "some", "one"),
+
+    quant: $ => choice("all", "no", "sum"), // TODO fix overlap
+    decl: $ => seq(optional("disj"), comma_separated($.name), ":", optional("disj"), $.expr),
+
     comment: _ => token(choice(
       seq("//", /[^\\\n]*/), // line comment
       // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
