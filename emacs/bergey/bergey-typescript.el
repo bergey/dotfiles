84bddfec104094c1e03f3bb3f2d1ad308d9d9b9b
Daniel Bergey <daniel.bergey@braze.com>
2025-08-19 10:05:21 -0400

    emacs: typescript-navigate-imports

--- emacs/bergey/bergey-typescript.el
+++ emacs/bergey/bergey-typescript.el
@@ -1,13 +1,24 @@
 (use-package typescript-mode :ensure t
   :mode "\\.ts"
   :bind (:map typescript-mode-map
-              ("C-c C-," . prettier-prettify))
+              ("C-c C-," . prettier-prettify)
+              ("C-c i" . bergey/typescript-navigate-imports)
+              )
   :custom
   (eglot-events-buffer-config '(:size 0 :format full))
   (eglot-sync-connect nil)
   (eglot-autoshutdown t)
   (eglot-send-changes-idle-time 3)
   (eglot-ignored-server-capabilities '(:documentHighlightProvider))
+
+  (defun bergey/typescript-navigate-imports ()
+    "move point to the beginning of the first import line"
+    (interactive)
+    (xref-push-marker-stack) ;; so we can return
+    (goto-char (point-min))
+    (re-search-forward "^import")
+    (goto-char (line-beginning-position))
+    )
   )
 
 (use-package add-node-modules-path
