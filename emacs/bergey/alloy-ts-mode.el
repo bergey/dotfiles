f5b27c83215ef79cc6c4458ef27fc0c2c26188b1
Daniel Bergey <daniel.bergey@braze.com>
2025-08-28 08:39:39 -0400

    alloy emacs mode: first highlight, indent rules

--- emacs/bergey/alloy-ts-mode.el
+++ emacs/bergey/alloy-ts-mode.el
@@ -23,6 +23,14 @@
               (apply #'treesit-font-lock-rules alloy-font-lock-rules))
   (setq-local treesit-simple-indent-rules alloy-indent-rules)
 
+  (setq-local treesit-font-lock-feature-list
+              '((comment))
+              ;; '((comment)
+              ;;   (paragraph logical temporal)
+              ;;   (prime relational)
+              ;;   (operator))
+              )
+
   (treesit-major-mode-setup)
   )
 
@@ -62,13 +70,24 @@
               (?\t "    ")
               (?\r "    ")
               (?\f "    "))
-      )
-    (modify-syntax-entry (car it) (cadr it) syntax-table))
-  syntax-table
+      (modify-syntax-entry (car it) (cadr it) syntax-table))
+    syntax-table)
   "Syntax table used in `alloy-mode' buffers.")
 
-(setq-local tree-sit-font-lock-feature-list
-            '((comment)
-              (paragraph logical temporal)
-              (prime relational)
-              (operator)))
+(defvar alloy-font-lock-rules
+  '(
+    :language alloy
+    :override t
+    :feature comment
+    ((comment) @font-lock-comment-face)
+    ))
+
+(defvar alloy-indent-rules
+  '((alloy
+     ;; this indents relative to the opening { not to the fact / pred introing the block
+     ;; also it doesn't cover sig, which has it's own {} production
+     ((parent-is "block") parent 2)
+     ;; paragraph is anonymous in the grammar, so list all the options
+     ((node-is "sig") column-0 0)
+     ((node-is "fact") column-0 0)
+     )))
