b0e14177bacbad8ebcf8296cb42a322318a28e2c
Daniel Bergey <bergey@teallabs.org>
2026-02-17 14:08:14 -0500

    emacs: firefox, not chromium

--- emacs/bergey/bergey-os.el
+++ emacs/bergey/bergey-os.el
@@ -3,7 +3,7 @@
 ;; Use system default web browser
 (if (memq system-type '(gnu gnu/linux))
     (setq browse-url-browser-function 'browse-url-generic
-          browse-url-generic-program "chromium")
+          browse-url-generic-program "firefox")
   ;; browse-url-default-browser calls lynx even if not installed, on Debian, -- 2017-02-23
   (setq browse-url-browser-function 'browse-url-default-browser)
   )
