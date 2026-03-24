0729c075a39a4056901fc6c39f01b16a589bd8cd
Daniel Bergey <bergey@teallabs.org>
2026-03-24 05:15:59 -0400

    emacs theme: define faces regardless of display capability
    I never run emacs on a 1-color VT

--- emacs/bergey-theme.el
+++ emacs/bergey-theme.el
@@ -17,18 +17,18 @@
  '(fixed-pitch ((t (:family "Inconsolata"))))
  '(italic ((t (:underline nil :slant italic))))
  '(monospace ((t (:family "Inconsolata"))))
- '(cursor ((((class color) (min-colors 89)) (:background "thistle"))))
+ '(cursor ((t (:background "thistle"))))
 
- '(error ((((class color) (min-colors 89)) (:foreground "salmon1"))))
- '(warning ((((class color) (min-colors 89)) (:foreground "orange"))))
- '(success ((((class color) (min-colors 89)) (:foreground "yellow green"))))
- '(highlight ((((class color) (min-colors 89)) (:foreground "white" :background "dark green"))))
- '(region ((((class color) (min-colors 89)) (:foreground "black" :background "chartreuse1"))))
- '(isearch ((((class color) (min-colors 88) (background dark)) (:background "palevioletred2" :foreground "black"))))
- '(lazy-highlight ((((class color) (min-colors 88) (background dark)) (:background "paleturquoise4" :foreground "black"))))
- '(lazy-highlight ((((class color) (min-colors 89)) (:background "gray25"))))
- '(link ((((class color) (min-colors 89)) (:foreground "cyan" :background "#134"))))
- '(link-visited ((((class color) (min-colors 89)) (:foreground "#59c" :background "#134"))))
+ '(error ((t (:foreground "salmon1"))))
+ '(warning ((t (:foreground "orange"))))
+ '(success ((t (:foreground "yellow green"))))
+ '(highlight ((t (:foreground "white" :background "dark green"))))
+ '(region ((t (:foreground "black" :background "chartreuse1"))))
+ '(isearch ((t (:background "palevioletred2" :foreground "black"))))
+ '(lazy-highlight ((t (:background "paleturquoise4" :foreground "black"))))
+ '(lazy-highlight ((t (:background "gray25"))))
+ '(link ((t (:foreground "cyan" :background "#134"))))
+ '(link-visited ((t (:foreground "#59c" :background "#134"))))
  '(secondary-selection ((t (:background "paleturquoise" :foreground "black"))))
 
  ;; programming language syntax, general
@@ -39,41 +39,39 @@
  '(smerge-refined-added ((t (:foreground"#005000"))))
  '(eglot-highlight-symbol-face ((t (:inherit nil) (:underline t))))
 
- `(font-lock-builtin-face ((((class color) (min-colors 89)) (:foreground ,b-indigo))))
+ `(font-lock-builtin-face ((t (:foreground ,b-indigo))))
  `(font-lock-comment-face ((t (:foreground ,dark-red))))
  `(font-lock-constant-face ((t (:foreground ,b-cyan))))
  `(font-lock-keyword-face ((t (:foreground ,b-indigo))))
  `(font-lock-preprocessor-face ((t (:inherit font-lock-builtin-face :foreground ,b-cyan))))
  `(font-lock-string-face ((t (:foreground ,b-orange))))
  `(font-lock-type-face ((t (:foreground ,b-violet))))
- '(font-lock-function-name-face ((((class color) (min-colors 89)) (:foreground "white"))))
- '(font-lock-variable-name-face ((((class color) (min-colors 89)) (:foreground "white"))))
+ '(font-lock-function-name-face ((t (:foreground "white"))))
+ '(font-lock-variable-name-face ((t (:foreground "white"))))
  `(font-lock-doc-face ((t (:foreground ,b-green))))
- '(font-lock-regexp-grouping-backslash ((((class color) (min-colors 89)) (:foreground "turquoise"))))
- '(font-lock-regexp-grouping-construct ((((class color) (min-colors 89)) (:foreground "turquoise"))))
+ '(font-lock-regexp-grouping-backslash ((t (:foreground "turquoise"))))
+ '(font-lock-regexp-grouping-construct ((t (:foreground "turquoise"))))
 
  '(fic-face ((t (:background "chocolate1" :foreground "black"))))
  '(mmm-default-submode-face ((t (:background "gray1"))))
- '(show-paren-match ((((class color) (background dark)) (:background "steelblue3" :foreground "black"))))
- '(show-paren-mismatch ((((class color)) (:background "purple" :foreground "black"))))
- '(whitespace-line ((((class color) (background dark)) (:underline "DeepSkyBlue" :foreground nil :background nil))))
- '(highlight-indent-guides-even-face ((t (:family "Inconsolata" :background "#103"))))
- '(highlight-indent-guides-odd-face ((t (:family "Inconsolata" :background "gray10"))))
+ '(show-paren-match ((t (:background "steelblue3" :foreground "black"))))
+ '(show-paren-mismatch ((t (:background "purple" :foreground "black"))))
+ '(whitespace-line ((t (:underline "DeepSkyBlue" :foreground nil :background nil))))
 
  ;; syntax for particular languages
  `(web-mode-function-call-face ((t (:foreground ,b-green))))
  '(font-mediawiki-sedate-face ((t (:foreground "gold"))) t)
- '(highlight-quoted-quote ((((class color) (min-colors 89)) (:foreground "white"))))
+ '(highlight-quoted-quote ((t (:foreground "white"))))
  '(highlight-quoted-symbol ((t (:foreground "#95f"))))
 ;; `(haskell-constructor-face ((t (:foreground ,b-red))))
- '(ledger-font-xact-highlight-face ((((class color) (background dark)) (:background "black"))))
- '(markdown-bold-face ((((class color) (background dark)) (:foreground "red" :inherit bold))))
+ '(ledger-font-xact-highlight-face ((t (:background "black"))))
+ '(markdown-bold-face ((t (:foreground "red" :inherit bold))))
  '(markdown-code-face ((t (:inherit org-verbatim))))
  '(markdown-table-face ((t (:inherit org-table))))
- '(proof-locked-face ((((class color) (background dark)) (:background "#333"))))
- '(shm-quarantine-face ((((class color) (background dark)) (:background "saddle brown")))) ; structured haskell mode
- '(web-mode-html-attr-name-face ((((class color) (background dark)) (:foreground "white"))))
- '(web-mode-html-tag-face ((((class color) (background dark)) (:foreground "red"))))
+ '(proof-locked-face ((t (:background "#333"))))
+ '(shm-quarantine-face ((t (:background "saddle brown")))) ; structured haskell mode
+ '(web-mode-html-attr-name-face ((t (:foreground "white"))))
+ '(web-mode-html-tag-face ((t (:foreground "red"))))
 
  ;; outlines / headings
  '(outline-1 ((t (:foreground "#fe0b54")))) ;; lch 55 87 21
@@ -84,30 +82,30 @@
  '(outline-6 ((t (:foreground "#ea84fe")))) ;; lch 70 70 320
 
  ;; nested parens
-'(rainbow-delimiters-depth-1-face ((t (:foreground "#fe0b54")))) ;; lch 55 87 21
-'(rainbow-delimiters-depth-2-face ((t (:foreground "#f6bb2b")))) ;; lch 80 75 80
-'(rainbow-delimiters-depth-3-face ((t (:foreground "#0be37a")))) ;; lch 80 75 150
-'(rainbow-delimiters-depth-4-face ((t (:foreground "#13f2f8")))) ;; lch 87 50 200
-'(rainbow-delimiters-depth-5-face ((t (:foreground "#5e84fe")))) ;; lch 57 64 282
-'(rainbow-delimiters-depth-6-face ((t (:foreground "#ea84fe")))) ;; lch 70 70 320
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#fe0b54")))) ;; lch 55 87 21
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#f6bb2b")))) ;; lch 80 75 80
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#0be37a")))) ;; lch 80 75 150
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#13f2f8")))) ;; lch 87 50 200
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#5e84fe")))) ;; lch 57 64 282
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#ea84fe")))) ;; lch 70 70 320
 
  ;;; other, non-programming modes
- '(company-tooltip ((((class color) (background dark)) (:background "grey40" :foreground "white"))))
- '(company-tooltip-selection ((((class color) (background dark)) (:background "grey20" :foreground "white"))))
- '(dired-directory ((((class color) (background dark)) (:foreground "gold"))))
- '(git-annex-dired-annexed-available ((((class color) (background dark)) (:foreground "spring green"))))
- '(git-annex-dired-annexed-unavailable ((((class color) (background dark)) (:foreground "chocolate"))))
+ '(company-tooltip ((t (:background "grey40" :foreground "white"))))
+ '(company-tooltip-selection ((t (:background "grey20" :foreground "white"))))
+ '(dired-directory ((t (:foreground "gold"))))
+ '(git-annex-dired-annexed-available ((t (:foreground "spring green"))))
+ '(git-annex-dired-annexed-unavailable ((t (:foreground "chocolate"))))
  '(magit-item-highlight ((t (:background "#101010" :foreground "cyan"))))
 
  ;; org-mode
  '(org-verbatim ((t (:foreground "white"))))
  `(org-code ((t (:foreground ,dark-orange))))
  '(org-column ((t (:family "Inconsolata" :height 90))))
- '(org-habit-alert-face ((((class color) (background dark)) (:foreground "black" :background "darkgoldenrod"))))
- '(org-tag ((((class color) (background dark)) (:background "#1f004d"))))
+ '(org-habit-alert-face ((t (:foreground "black" :background "darkgoldenrod"))))
+ '(org-tag ((t (:background "#1f004d"))))
  '(org-table ((t (:foreground "gray70" :inherit fixed-pitch))))
 
- '(erc-notice-face ((((class color) (background dark)) (:foreground "LightSteelBlue4"))))
+ '(erc-notice-face ((t (:foreground "LightSteelBlue4"))))
 
  '(mode-line ((t (:foreground "#ea84fe" :background "gray10"))))
  '(mode-line-inactive ((t (:background "gray20"))))
