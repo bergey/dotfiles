f9ba4588c88237b4c6e9b31cedd19fd9cb117cf5
Daniel Bergey <bergey@teallabs.org>
2026-03-24 18:12:07 -0400

    emacs theme: draw more from my color wheel

--- emacs/bergey-theme.el
+++ emacs/bergey-theme.el
@@ -2,7 +2,6 @@
   "Created 2013-01-18.")
 
 ;; TODO remove unused faces
-;; TODO consolidate colors again
 
 ;; must match rainbow-r-colors-alist at bottom of file or these names will not get highlighted
 (setq
@@ -16,7 +15,9 @@
    ("b-violet" . "#ea84fe")
    ("dark-red" . "#8c0437")
    ("dark-orange" . "#8f4a31")
+   ("dark-green" . "#246c01")
    ("dark-blue" . "#086783")
+   ("dark-indigo" . "#475aac")
    ))
 
 (defmacro bergey/theme (name &rest body)
@@ -59,33 +60,31 @@
  (font-lock-regexp-grouping-backslash "turquoise")
  (font-lock-regexp-grouping-construct "turquoise")
 
- (fic-face (:background "chocolate1" :foreground "black"))
+ (fic-face (:background "dark-blue" :foreground "black"))
  (mmm-default-submode-face (:background "gray1"))
- (show-paren-match (:background "steelblue3" :foreground "black"))
- (show-paren-mismatch (:background "purple" :foreground "black"))
- (whitespace-line (:underline "DeepSkyBlue" :foreground nil :background nil))
+ (show-paren-match (:background "dark-green" :foreground "black"))
+ (show-paren-mismatch (:background "b-violet" :foreground "black"))
+ (whitespace-line (:underline "dark-blue" :foreground nil :background nil))
 
  ;; syntax for particular languages
  (web-mode-function-call-face "b-green")
  (font-mediawiki-sedate-face "gold")
  (highlight-quoted-quote "white")
- (highlight-quoted-symbol "#95f")
+ (highlight-quoted-symbol "b-violet")
  ;; (haskell-constructor-face (:foreground ,b-red))
  (ledger-font-xact-highlight-face (:background "black"))
- (markdown-bold-face (:foreground "red" :inherit 'bold))
+ (markdown-bold-face (:foreground "b-red" :inherit 'bold))
  (markdown-code-face (:inherit 'org-verbatim))
  (markdown-table-face (:inherit 'org-table))
  (proof-locked-face (:background "#333"))
  (shm-quarantine-face (:background "saddle brown")) ; structured haskell mode
  (web-mode-html-attr-name-face "white")
- (web-mode-html-tag-face "red")
+ (web-mode-html-tag-face "b-red")
  ;; other, non-programming modes
  (company-tooltip (:background "grey40" :foreground "white"))
  (company-tooltip-selection (:background "grey20" :foreground "white"))
- (dired-directory "gold")
- (git-annex-dired-annexed-available "spring green")
- (git-annex-dired-annexed-unavailable "chocolate")
- (magit-item-highlight (:background "#101010" :foreground "cyan"))
+ (dired-directory "b-orange")
+ (magit-item-highlight (:background "#101010" :foreground "b-cyan"))
 
  ;; org-mode
  (org-verbatim "white")
@@ -111,21 +110,21 @@
  (rainbow-delimiters-depth-5-face "b-indigo") ;; lch 57 64 282
  (rainbow-delimiters-depth-6-face "b-violet") ;; lch 70 70 320
 
- (error "salmon1")
- (warning "orange")
- (success "yellow green")
- (highlight (:foreground "white" :background "dark green"))
- (region (:foreground "black" :background "chartreuse1"))
+ (error "b-red")
+ (warning "b-orange")
+ (success "b-green")
+ (highlight (:foreground "white" :background "dark-green"))
+ (region (:foreground "black" :background "dark-indigo"))
  (isearch (:background "palevioletred2" :foreground "black"))
  (lazy-highlight (:background "paleturquoise4" :foreground "black"))
  (lazy-highlight (:background "gray25"))
- (link (:foreground "cyan" :background "#134"))
- (link-visited (:foreground "#59c" :background "#134"))
+ (link (:foreground "b-cyan" :background "#134"))
+ (link-visited (:foreground "b-indigo" :background "#134"))
  (secondary-selection (:background "paleturquoise" :foreground "black"))
- (diff-added (:inherit 'diff-changed :foreground "green"))
- (diff-removed (:inherit 'diff-changed :foreground "red"))
- (ediff-current-diff-A (:foreground"light gray" :background "#553333"))
- (ediff-current-diff-B (:foreground"light gray" :background "#335533"))
+ (diff-added (:inherit 'diff-changed :foreground "b-green"))
+ (diff-removed (:inherit 'diff-changed :foreground "b-red"))
+ (ediff-current-diff-A (:foreground"light gray" :background "dark-orange"))
+ (ediff-current-diff-B (:foreground"light gray" :background "dark-green"))
  (smerge-refined-added (:foreground"#005000"))
  (eglot-highlight-symbol-face (:inherit nil) (:underline t))
  (erc-notice-face "LightSteelBlue4")
@@ -151,7 +150,7 @@
 ;; (set-face-attribute 'powerline-active1 nil :inherit nil)
 
 ;; Local Variables:
-;; eval: (rainbow-mode)
 ;; rainbow-r-colors: t
-;; rainbow-r-colors-alist: (("b-red" . "#fe0b54") ("b-orange" . "#f6bb2b") ("b-green" . "#0be37a") ("b-cyan" . "#13f2f8") ("b-indigo" . "#5e84fe") ("b-violet" . "#ea84fe") ("dark-red" . "#8c0437") ("dark-orange" . "#8f4a31") ("dark-blue" . "#086783"))
+;; rainbow-r-colors-alist: (("b-red" . "#fe0b54") ("b-orange" . "#f6bb2b") ("b-green" . "#0be37a") ("b-cyan" . "#13f2f8") ("b-indigo" . "#5e84fe") ("b-violet" . "#ea84fe") ("dark-red" . "#8c0437") ("dark-orange" . "#8f4a31") ("dark-green" . "#246c01") ("dark-blue" . "#086783") ("dark-indigo" . "#475aac"))
+;; eval: (rainbow-mode)
 ;; End:
