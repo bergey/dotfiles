c41735426962958a0b22eb81e49aa940844f9bcf
Daniel Bergey <bergey@teallabs.org>
2026-03-24 06:11:53 -0400

    emacs: ruby config in use-package, load at need

--- emacs/bergey/bergey-ruby.el
+++ emacs/bergey/bergey-ruby.el
@@ -1,18 +1,92 @@
 ;; starting point from https://github.com/amake/.emacs.d/blob/master/init.el
 
 (use-package ruby-mode :ensure t
+  :mode (rx ".rb" string-end)
   :custom
   (ruby-insert-encoding-magic-comment nil "Not needed in Ruby 2")
+  :bind
+  (:map ruby-mode-map
+        ("C-c i" . bergey/ruby-navigate-imports)
+        ("C-c m" . bergey/ruby-yank-module-name)
+        ("C-c c" . bergey/ruby-yank-class-name)
+        ("C-c C-d" . bergey/ruby-do-outline)
+        )
+
   :config
   (setq ruby-mode-hook
         '(
           (lambda () (setq-local flymake-diagnostic-functions '(ruby-flymake-auto eglot-flymake-backend)))
           whitespace-mode
+          rubocop-mode
+          bergey/try-ruby-mode
           ))
-  ;; :bind (:map ruby-mode-map ("M-." . bergey/ruby-rg-definition))
+
+  (defun bergey/ruby-navigate-imports ()
+    "move point to the beginning of the first import line"
+    (interactive)
+    (xref-push-marker-stack) ;; so we can return
+    (goto-char (point-min))
+    (re-search-forward "^\\(require\\|load\\)")
+    (goto-char (line-beginning-position))
+    )
+
+  (defun bergey/ruby-yank-module-name ()
+    "copy the module name to the kill ring"
+    (interactive)
+    (bergey/ruby-yank-class-or-module-name t nil))
+
+  (defun bergey/ruby-yank-class-or-module-name (include-module include-class)
+    "Find all occurrences of the regex `module [a-z]` before the first occurrence of the regex `^ *class` and concatenate the matches."
+    (let ((buf (current-buffer))
+          (case-fold-search nil)
+          )
+      (save-excursion
+        (let ((search-end (point))
+              (words nil)
+              (regex (cond
+                      ;; these differ only by keyword
+                      ((and include-module include-class) "^ *\\(?:class\\|module\\) \\([a-zA-Z0-9]+\\)")
+                      (include-module "^ *module \\([a-zA-Z0-9]+\\)")
+                      (include-class "^ *class \\([a-zA-Z0-9]+\\)"))))
+          (goto-char (point-min))
+          (while (re-search-forward regex search-end t)
+            (setq words (cons (match-string-no-properties 1) words)))
+          (let ((ret (s-join "::"(reverse words))))
+            (kill-new ret)
+            (message "%s" ret )
+            )))))
+
+  (defun bergey/ruby-yank-class-name (arg)
+    "Find all occurrences of the regex `module [a-z]` before the first occurrence of the regex `^ *class` and concatenate the matches."
+    (interactive "P")
+    (bergey/ruby-yank-class-or-module-name arg t))
+
+  (defun bergey/ruby-do-outline ()
+    "occur buffer with all do block headings"
+    (interactive)
+    (occur "do$"))
+
+  (defun bergey/ruby-align-left-class ()
+    "align the first class keyword to the leftmost column of window"
+    (interactive)
+    (save-excursion
+      (goto-char (point-min))
+      (search-forward-regexp " *\\(class\\)")
+      (goto-char (match-beginning 1))
+      (bergey/scroll-align-left))
+    )
+
+  (define-skeleton braze-ff-company ""
+    "Feature Flipper: "
+    "FeatureFlipperStore.get(company_id).feature_on?(:" str _ ")"
+    )
+
+  (define-skeleton braze-ff-global "" nil
+    "FeatureFlipperStore.global.feature_on?(:" _  ")")
   )
 
 (use-package inf-ruby
+  :commands inf-ruby
   :bind
   (:map inf-ruby-minor-mode-map
 
@@ -38,126 +112,51 @@
   )
 
 (use-package rspec-mode :ensure t
+  :commands rspec-mode
   :init
   (defun bergey/try-ruby-mode ()
     (if (s-ends-with? "spec.rb" (buffer-file-name))
         (rspec-mode)))
-  (add-to-list 'ruby-mode-hook #'bergey/try-ruby-mode)
+
+  :bind
+  (:map rspec-mode-map
+        ("C-c r" . bergey/ruby-yank-rspec-command)
+        ("C-c C-o" . bergey/ruby-rspec-outline)
+        )
+
+  :config
+  (defun bergey/ruby-yank-rspec-command (arg)
+    "copy an rspec command which can be run in the shell to run the tests in the current file"
+    (interactive "p")
+    (let ((prefix (if (s-match "/domains/" (buffer-file-name)) "^\\(.*\\)domains/" "^\\(.*\\)spec/")))
+      (kill-new (s-replace-regexp prefix (if (= arg 4) " " "bundle exec rspec ") (buffer-file-name) nil nil 1)))
+    )
+
+  (defun bergey/ruby-rspec-outline ()
+    "occur buffer with the main headings of an rspec file"
+    (interactive)
+    (occur "^ *\\(context\\|\\(RSpec\\.\\)?describe\\|shared_examples\\|it_behaves_like\\)\\>")
+    )
   )
 
 (use-package feature-mode :ensure t
   :mode "\\.feature"
   :config
-  (bind-key "M-." #'picklebush-line feature-mode-map)
+  :bind
+  (:map feature-mode-map)
+  ("M-." . picklebush-line)
   )
-
-(defun bergey/ruby-navigate-imports ()
-  "move point to the beginning of the first import line"
-  (interactive)
-  (xref-push-marker-stack) ;; so we can return
-  (goto-char (point-min))
-  (re-search-forward "^\\(require\\|load\\)")
-  (goto-char (line-beginning-position))
-  )
-(bind-key "C-c i" #'bergey/ruby-navigate-imports ruby-mode-map)
-
-(defun bergey/ruby-yank-module-name ()
-  "copy the module name to the kill ring"
-  (interactive)
-  (bergey/ruby-yank-class-or-module-name t nil))
-(bind-key "C-c m" #'bergey/ruby-yank-module-name ruby-mode-map)
-
-(defun bergey/ruby-yank-class-or-module-name (include-module include-class)
-  "Find all occurrences of the regex `module [a-z]` before the first occurrence of the regex `^ *class` and concatenate the matches."
-  (let ((buf (current-buffer))
-        (case-fold-search nil)
-        )
-    (save-excursion
-      (let ((search-end (point))
-            (words nil)
-            (regex (cond
-                    ;; these differ only by keyword
-                    ((and include-module include-class) "^ *\\(?:class\\|module\\) \\([a-zA-Z0-9]+\\)")
-                    (include-module "^ *module \\([a-zA-Z0-9]+\\)")
-                    (include-class "^ *class \\([a-zA-Z0-9]+\\)"))))
-        (goto-char (point-min))
-        (while (re-search-forward regex search-end t)
-          (setq words (cons (match-string-no-properties 1) words)))
-        (let ((ret (s-join "::"(reverse words))))
-          (kill-new ret)
-          (message "%s" ret )
-          )))))
-
-(defun bergey/ruby-yank-class-name (arg)
-  "Find all occurrences of the regex `module [a-z]` before the first occurrence of the regex `^ *class` and concatenate the matches."
-  (interactive "P")
-  (bergey/ruby-yank-class-or-module-name arg t))
-(bind-key "C-c c" #'bergey/ruby-yank-class-name ruby-mode-map)
-
-(defun bergey/ruby-yank-rspec-command (arg)
-  "copy an rspec command which can be run in the shell to run the tests in the current file"
-  (interactive "p")
-  (let ((prefix (if (s-match "/domains/" (buffer-file-name)) "^\\(.*\\)domains/" "^\\(.*\\)spec/")))
-    (kill-new (s-replace-regexp prefix (if (= arg 4) " " "bundle exec rspec ") (buffer-file-name) nil nil 1)))
-  )
-(bind-key "C-c r" #'bergey/ruby-yank-rspec-command rspec-mode-map)
-
-(defun bergey/ruby-rg-definition ()
-  (interactive)
-  (let ((case-fold-search nil )
-        (table (copy-syntax-table (syntax-table))))
-    (modify-syntax-entry ?_ "w" table)
-    (with-syntax-table table
-      (let* ((name (thing-at-point 'word 'no-attributes))
-             (is-const (s-matches? "^[A-Z_]+$" name))
-             (is-class (s-matches? "^[A-Z]" name))
-             (query (cond
-                     (is-const (format "%s *=" name))
-                     (is-class (format "(class|module) %s" name))
-                     (t (format "def (self.)?%s" name)))))
-        (message "%s" query)
-        (rg-run query "ruby" (rg-project-root buffer-file-name) )
-        ))))
 
 (use-package rubocop :ensure t
+  :commands rubocop-mode
+  :custom
+  (rubocop-prefer-system-executable t)
   :config
-    (add-hook 'ruby-mode-hook #'rubocop-mode)
-    (setq rubocop-prefer-system-executable t)
-  ;; maybe also rubocop-format-current-file ?
   :bind (:map ruby-mode-map ("C-c C-," . rubocop-autocorrect-current-file))
   )
 
-(defun bergey/ruby-rspec-outline ()
-    "occur buffer with the main headings of an rspec file"
-    (interactive)
-    (occur "^ *\\(context\\|\\(RSpec\\.\\)?describe\\|shared_examples\\|it_behaves_like\\)\\>")
-    )
-(bind-key "C-c C-o" #'bergey/ruby-rspec-outline rspec-mode-map)
-
-(defun bergey/ruby-do-outline ()
-  "occur buffer with all do block headings"
-  (interactive)
-  (occur "do$"))
-(bind-key "C-c C-d" #'bergey/ruby-do-outline)
-
-(defun bergey/ruby-align-left-class ()
-  "align the first class keyword to the leftmost column of window"
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (search-forward-regexp " *\\(class\\)")
-    (goto-char (match-beginning 1))
-    (bergey/scroll-align-left))
+(use-package rbs-mode
+  :mode (rx ".rbs" string-end)
   )
-
-(define-skeleton braze-ff-company ""
-  "Feature Flipper: "
- "FeatureFlipperStore.get(company_id).feature_on?(:" str _ ")"
-  )
-
-(define-skeleton braze-ff-global "" nil
-  "FeatureFlipperStore.global.feature_on?(:" _  ")")
-
-(use-package rbs-mode)
 
 (provide 'bergey-ruby)
