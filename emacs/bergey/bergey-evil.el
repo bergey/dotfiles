cb7c821409f83ecdbfc6469e12dcdd82b339eba1
Daniel Bergey <bergey@teallabs.org>
2026-03-24 18:46:00 -0400

    evil: use use-package more idiomatically

--- emacs/bergey/bergey-evil.el
+++ emacs/bergey/bergey-evil.el
@@ -1,15 +1,31 @@
 (use-package evil
+  :bind (
+         :map evil-normal-state-map
+         ("s" . evil-forward-char)
+         ("Q" call-last-kbd-macro)
+         :map evil-visual-state-map
+         ("Q" call-last-kbd-macro)
+         :map evil-motion-state-map
+         ("M-<down>" evil-scroll-line-down )
+         ("M-<up>" evil-scroll-line-up)
+         ;; bindings that should only apply in certain evil states, not all
+         :map evil-insert-state-map
+         (evil-toggle-key evil-emacs-state)
+         ("<escape>" evil-normal-state)
+         ;; replaces 'evil-repeat-pop-next'; I haven't decided where to rebind that
+         (define-key evil-normal-state-map (kbd "M-.") nil)
+         )
   :init
   (use-package undo-fu
     :config
     (setq evil-undo-system 'undo-fu)
     )
-  (setq evil-want-keybinding nil)
-  (setq evil-want-minibuffer t)
 
   :custom
-  (evil-insert-state-cursor '(bar . 1))
   (evil-cross-lines t)
+  (evil-insert-state-cursor '(bar . 1))
+  (evil-want-keybinding nil)
+  (evil-want-minibuffer t)
 
   ;; This makes the cursor position more like emacs position.  It also
   ;; makes `sp-forward-sexp' work, for some reason.  I only care about
@@ -17,6 +33,7 @@
   ;; prefer `i' to `a', so that `i<esc>' brings you back where you
   ;; started.
   (evil-move-cursor-back nil)
+
   :config
   (evil-mode 1)
 
@@ -27,27 +44,10 @@
             (define-key evil-visual-state-map (kbd key-string) nil)
             )
           '("C-o" "C-." "TAB" ";"))
-
   (setcdr evil-insert-state-map nil)
   (define-key evil-motion-state-map (kbd "RET") nil)
 
-  ;; bindings that should only apply in certain evil states, not all
-  (define-key evil-insert-state-map (kbd evil-toggle-key) 'evil-emacs-state)
-  (define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
-  (define-key evil-normal-state-map (kbd "Q") 'call-last-kbd-macro)
-  (define-key evil-visual-state-map (kbd "Q") 'call-last-kbd-macro)
-  (bind-keys
-   :map evil-normal-state-map
-   ("s" . evil-forward-char))
-
-  ;; replaces 'evil-repeat-pop-next'; I haven't decided where to rebind that
-  (define-key evil-normal-state-map (kbd "M-.") nil)
-
   (evil-define-key 'normal magit-blame-mode-map "q" 'magit-blame-quit)
-
-  ;; new bindings
-  (bind-key "M-<down>" 'evil-scroll-line-down evil-motion-state-map)
-  (bind-key "M-<up>" 'evil-scroll-line-up evil-motion-state-map)
 
   ;;; per-mode customization
   (add-hook 'org-capture-mode-hook 'evil-insert-state)
@@ -58,14 +58,16 @@
   ;; commands (using vim in terminal in `screen').
   (evil-ex-define-cmd "q" 'kill-this-buffer)
 
-  (setq-default evil-shift-width 2))
+  (setq-default evil-shift-width 2)
+  )
 
 (use-package evil-collection
   ;; magit, rg, maybe dired benefit from this
   :after evil
   :diminish evil-collection-unimpaired-mode
+  :custom
+  (bergey/evil-collection-original-mode-list evil-collection-mode-list)
   :config
-  (setq bergey/evil-collection-original-mode-list evil-collection-mode-list)
   (evil-collection-init)
   (evil-define-key 'normal Info-mode-map (kbd "M-h") nil)
   )
