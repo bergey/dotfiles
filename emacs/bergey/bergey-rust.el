d6a5835b35f0d74cdde18f86e2bf8c53d649312b
Daniel Bergey <bergey@teallabs.org>
2026-03-24 08:42:56 -0400

    emacs rust: use-package, rx

--- emacs/bergey/bergey-rust.el
+++ emacs/bergey/bergey-rust.el
@@ -1,5 +1,6 @@
 (use-package rust-mode :ensure t
-  :mode ("\\.rs\'" . rust-mode)
+  :mode ((rx ".rs" string-end) . rust-mode)
+  :dash "Rust"
   :bind (:map rust-mode-map
               ("C-c C-," . rust-format-buffer)
               ("C-c i" . bergey/rust-navigate-imports)
@@ -8,42 +9,39 @@
   :config
   (add-hook 'rust-mode-hook #'electric-pair-local-mode)
   (add-hook 'rust-mode-hook #'(lambda () (setq-local evil-shift-width 4)))
-  :dash "Rust"
+  (defun bergey/rust-navigate-imports()
+    "move point to the beginning of the first import line"
+    (interactive)
+    (xref-push-marker-stack) ;; so we can return
+    (goto-char (point-min))
+    (re-search-forward "^use")
+    (goto-char (line-beginning-position))
+    )
+
+  (defun bergey/rust-yank-module-name ()
+    "copy the module name to the kill ring"
+    (interactive)
+    (let* (
+           (package-root (file-name-directory (rust-buffer-project)))
+           (module-path-src (s-chop-prefix package-root (buffer-file-name)))
+           ;; TODO what about other top-level folders?
+           (module-path (s-chop-suffix ".rs" (s-chop-prefix "src/" module-path-src)))
+           (module-name (s-replace "/" "::" module-path))
+           )
+      (kill-new module-name))
+    )
+  (define-skeleton rust-insert-test ""
+    "name: "
+    > "#[test]\n"
+    > "fn " str "() {\n"
+    > _ "\n"
+    > "}")
   )
 
 (use-package flymake-clippy
   :hook (rust-mode . flymake-clippy-setup-backend))
 
 (use-package toml-mode :ensure t
-  :mode "\\.toml\\|Cargo.lock" )
-
-(defun bergey/rust-navigate-imports()
-  "move point to the beginning of the first import line"
-  (interactive)
-  (xref-push-marker-stack) ;; so we can return
-  (goto-char (point-min))
-  (re-search-forward "^use")
-  (goto-char (line-beginning-position))
-  )
-
-(defun bergey/rust-yank-module-name ()
-  "copy the module name to the kill ring"
-  (interactive)
-  (let* (
-         (package-root (file-name-directory (rust-buffer-project)))
-         (module-path-src (s-chop-prefix package-root (buffer-file-name)))
-         ;; TODO what about other top-level folders?
-         (module-path (s-chop-suffix ".rs" (s-chop-prefix "src/" module-path-src)))
-         (module-name (s-replace "/" "::" module-path))
-         )
-    (kill-new module-name))
-  )
-
-(define-skeleton rust-insert-test ""
-  "name: "
-  > "#[test]\n"
-  > "fn " str "() {\n"
-  > _ "\n"
-  > "}")
+  :mode (rx (or (seq ".toml" string-end) "Cargo.lock")))
 
 (provide 'bergey-rust)
