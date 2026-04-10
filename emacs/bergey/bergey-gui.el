c2dcfb503242c85ee5499848a196970f18dc62c8
Daniel Bergey <daniel.bergey@braze.com>
2026-04-10 12:18:31 -0400

    emacs: ansi-color-names-vector is obsolete
    my theme now sets the appropriate faces

--- emacs/bergey/bergey-gui.el
+++ emacs/bergey/bergey-gui.el
@@ -22,13 +22,6 @@ Besides the choice of face, it is the same as `buffer-face-mode'."
 ;; colors
 (measure-time "theme" (load-theme 'bergey t))
 
-;; set this before ansi-color is loaded, and it gets picked up automaticaly
-(setq ansi-color-names-vector
-      '["black" "red3" "green3" "yellow3" "deep sky blue" "magenta3" "turquoise" "gray90"]
-      )
-;; C-x C-e next line when testing changes to above
-;; (setq ansi-color-map (ansi-color-make-color-map))
-
 (use-package rainbow-mode )
 
 ;; clean up interface
