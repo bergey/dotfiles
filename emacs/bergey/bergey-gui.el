e94a0e925c17ed6d6b06e848eac9d3d646e83cd2
Daniel Bergey <daniel.bergey@braze.com>
2026-06-12 22:32:30 +0000

    emacs: if we can't find gentium, keep going

--- emacs/bergey/bergey-gui.el
+++ emacs/bergey/bergey-gui.el
@@ -1,7 +1,9 @@
 ;; fonts
 (measure-time "gentium"
               (setq gentium (-first (-partial '-contains? '("Gentium" "GentiumPlus" "Gentium Plus")) (font-family-list)))
-              (set-face-font 'default gentium)
+	      (if gentium
+		  (set-face-font 'default gentium)
+		(message "skipping gentium font"))
               )
 
 (defun monospace-mode (&optional arg)
